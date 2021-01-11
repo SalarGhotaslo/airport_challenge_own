b63ac16e746bf2fa0ac6007af2707ad20e50f669
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-11 19:41:02 +0000

    user story 3 completed

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,7 +2,20 @@
 
 # Creating a class airport
 class Airport
-  def land(plane); end
 
-  def take_off(plane); end
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+  end
+
+  def land(plane) 
+    raise 'Cannot land as airport is full' if @planes.length >= @capacity
+    @planes << plane
+  end
+
+  def take_off(plane)
+  end
 end
+
+
+
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,7 +3,7 @@
 require 'airport'
 
 describe Airport do
-  airport = Airport.new
+  airport = Airport.new(20)
 
   it 'should land plane at airport' do
     expect(airport).to respond_to(:land).with(1).argument
@@ -11,4 +11,12 @@ describe Airport do
   it 'plane should takeoff' do
     expect(airport).to respond_to(:take_off).with(1).argument
   end
+
+  it 'does not allow plane to land when airport is full' do
+    plane = double :plane
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land as airport is full'
+end
 end
