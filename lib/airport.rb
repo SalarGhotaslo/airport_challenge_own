addd16935674c5df7362367048050443199d6768
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-11 23:16:04 +0000

    refactored after previous step

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,20 +2,22 @@
 
 # Creating a class airport
 class Airport
-
   def initialize(capacity)
     @capacity = capacity
     @planes = []
   end
 
-  def land(plane) 
-    raise 'Cannot land as airport is full' if @planes.length >= @capacity
+  def land(plane)
+    raise 'Cannot land as airport is full' if full?
+
     @planes << plane
   end
 
-  def take_off(plane)
-  end
+  def take_off(plane); end
 end
 
+private
 
-
+def full?
+  @planes.length >= @capacity
+end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,19 +4,26 @@ require 'airport'
 
 describe Airport do
   airport = Airport.new(20)
+  let(:plane) { double :plane }
 
-  it 'should land plane at airport' do
-    expect(airport).to respond_to(:land).with(1).argument
-  end
-  it 'plane should takeoff' do
-    expect(airport).to respond_to(:take_off).with(1).argument
-  end
-
-  it 'does not allow plane to land when airport is full' do
-    plane = double :plane
-    20.times do
-      airport.land(plane)
+  describe '#land' do
+    it 'should land plane at airport' do
+      expect(airport).to respond_to(:land).with(1).argument
     end
-    expect { airport.land(plane) }.to raise_error 'Cannot land as airport is full'
+
+    context 'when at capacity' do
+    it 'does not allow plane to land' do
+      # This is called a double and used to display a class at certain times
+      20.times do
+        airport.land(plane)
+      end
+      expect { airport.land(plane) }.to raise_error 'Cannot land as airport is full'
+    end
+  end
 end
+  describe '#take_off' do
+    it 'plane should takeoff' do
+      expect(airport).to respond_to(:take_off).with(1).argument
+    end
+  end
 end
